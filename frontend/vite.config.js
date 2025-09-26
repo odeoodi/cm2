a02a3449821ddd8f11732eac7be06011a510ebcf
Tanvir Rahman Nibir <TANVIR.NIBIR@metropolia.fi>
2025-09-26 10:27:46 +0300

    Commiting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,31 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+plugins: [react()],
+server: {
+   port: 3000,
+   proxy: {
+      "/api": {
+      target: "http://localhost:4000",
+       changeOrigin: true,
+      //
       },
-    },
-  },
+   },
+},
 });
- 
-
-// export default defineConfig({
-  // plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:4000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
